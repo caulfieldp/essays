5d80b6dfb0296af5397e63bdd16d9731b525505d
Phoebe Caulfield <caulfield.phoebe@outlook.com>
2020-06-22 22:50:54 -0700

    Updates

---
+++ precache-manifest.0cb8732f868ae7ecee305eb88f1c9f31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffc7d7710f949c462f01ab51b06e40b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7bd3e3f1f3f590bf496",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "78b7ba99923c099f6966",
+    "url": "/static/js/2.3c433d67.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3c433d67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7bd3e3f1f3f590bf496",
+    "url": "/static/js/main.7326e693.chunk.js"
+  },
+  {
+    "revision": "9bd6d95d4762ce7ae5de",
+    "url": "/static/js/runtime-main.3c71fba0.js"
+  }
+]);
